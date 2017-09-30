33b177f7634d043bb31d17d02e692a0be7dc3c80
Adrien du Peloux <adrien.dupeloux@edhec.com>
2017-09-30 15:52:19 +0200

    fin de lexercice

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,20 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(safe_params)
+     if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def safe_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
